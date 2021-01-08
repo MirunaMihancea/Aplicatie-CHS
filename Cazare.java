c70f12ef8cb6d09bd5887f1987f99c1b7a979c28
MirunaMihancea <77130683+MirunaMihancea@users.noreply.github.com>
2021-01-08 10:48:46 +0200

    Add files via upload

---
+++ Cazare.java
@@ -0,0 +1,14 @@
+package com.example.authenticatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cazare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cazare);
+    }
+}
